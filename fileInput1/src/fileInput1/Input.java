bd1678d5b87f337f7498991e110a9aee8e5ed7bc
cynthia <calvocyn@gmail.com>
2020-03-19 09:58:01 -0500

    comment

--- fileInput1/src/fileInput1/Input.java
+++ fileInput1/src/fileInput1/Input.java
@@ -1,9 +1,40 @@
 package fileInput1;
 
+import java.io.*;
+
 public class Input {
 
 	public static void main(String[] args) {
-		
+		int count = 0, character, wordLength = 0;
+		double avgLength = 0.0;
+		String word = "";
+		try {
+			FileReader reader = new FileReader("file.txt");
+			BufferedReader br = new BufferedReader(reader);
+			
+			while((character = br.read()) != -1) {
+				char c = (char)character;
+				if((c >= 'a') && (c <= 'z') || (c >= 'A') && (c <= 'Z')) {
+					wordLength+=1;
+					word+=c;
+				}else {
+					avgLength+=wordLength;
+					avgLength = avgLength/2;
+					wordLength = 0;
+					count+=1;
+					System.out.print(word);
+					word = "";
+				}
+			}
+			
+			System.out.println("Average Length: " + avgLength);
+			System.out.println("Word Count: " + count);
+			
+			reader.close();
+			
+		}catch(Exception e) {
+			System.out.println("That doesn't work");
+		}
 
 	}
 
